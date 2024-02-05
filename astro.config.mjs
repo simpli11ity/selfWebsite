de1e9bbb02887c0d7209d8220d01a94b195451e4
simpli11ity <peter.swayne.12@gmail.com>
2024-02-05 02:07:25 +0000

    add base and replace site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://simpli11ity.github.io/selfWebsite/',
+	site: 'https://simpli11ity.github.io/',
+	base: '/selfWebsite',
 	integrations: [mdx(), sitemap()],
 });

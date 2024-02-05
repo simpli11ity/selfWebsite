a3aee9625bbb2d59c2f3987779bbae550991ec8a
simpli11ity <peter.swayne.12@gmail.com>
2024-02-05 02:04:35 +0000

    update website name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://simpli11ity.github.io/selfWebsite/',
 	integrations: [mdx(), sitemap()],
 });

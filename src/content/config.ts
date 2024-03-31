5d8f01d110f1276a66edee2ee2661dd44391d22f
simpli11ity <peter.swayne.12@gmail.com>
2024-03-31 22:01:45 +0100

    update path for travel

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,7 +26,21 @@ const projects = defineCollection({
 	}),
 });
 
+const travel = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
 export const collections = {
 	'blog': blog,
-	'projects': projects
+	'projects': projects,
+	'travel': travel
   };

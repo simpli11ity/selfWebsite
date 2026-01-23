dab5718b0aebb6b9fa0c0ae9bee51b34a347292d
Peter <peter.swayne.12@gmail.com>
2026-01-23 00:26:42 +0000

    te

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'blog';
+export const SITE_TITLE = 'ttttt';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const ROUTE_LOCAL = '/fonts/atkinson-bold.woff';
 export const ROUTE_ONLINE = '/fonts/atkinson-bold.woff'
